d06b5f0f45e2c1e6eab22ceee660314edf987027
kipmaiyo <danielkipmaiyo44@gmail.com>
2023-09-05 15:33:52 +0300

    recap

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    let Text = document.getElementById("text")
+    Text.textContent = "This is really cool!"
+  });
